07de9907aa38131dfeb5f5de348e1a36e79b6dfc
bashathadikela <bashacse1549@yahoo.com>
2020-09-11 16:49:29 +0530

    Add PowerVS Network Manager Spec tests
    display_name_method modified and removed default_blacklisted_event_names method

---
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/network_manager_spec.rb
@@ -0,0 +1,27 @@
+describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::NetworkManager do
+  context "ems" do
+    it "does not support network creation" do
+      ems = FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud)
+      expect(ems.supports_ems_network_new?).to eq(false)
+    end
+  end
+
+  context "singleton methods" do
+    it "returns the expected value for the description method" do
+      expect(described_class.description).to eq('IBM Power Systems Virtual Servers Network')
+    end
+
+    it "returns the expected value for the ems_type method" do
+      expect(described_class.ems_type).to eq('ibm_cloud_power_virtual_servers_network')
+    end
+
+    it "returns the expected value for the hostname_required? method" do
+      expect(described_class.hostname_required?).to eq(false)
+    end
+
+    it "returns the expected value for the display_name method" do
+      expect(described_class.display_name).to eq('Network Manager')
+      expect(described_class.display_name(2)).to eq('Network Managers')
+    end
+  end
+end
