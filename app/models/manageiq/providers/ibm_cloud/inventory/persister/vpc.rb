76b4e75fb6990245ea6e06b2e3a043c11a724e24
Omar Afifi <Omar.Afifi@ibm.com>
2020-09-16 11:56:08 -0400

    removing uneeded comment, and adding new line

--- app/models/manageiq/providers/ibm_cloud/inventory/persister/vpc.rb
+++ app/models/manageiq/providers/ibm_cloud/inventory/persister/vpc.rb
@@ -11,24 +11,6 @@ class ManageIQ::Providers::IbmCloud::Inventory::Persister::VPC < ManageIQ::Provi
 
   def initialize_inventory_collections
     add_collection(cloud, :vms)
-    # add_cloud_collection(:vms) do |builder|
-    #   builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    # end
-    # add_collection(cloud, :vms) do |builder|
-    #   builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    #   builder.add_properties(:parent => cloud_manager)
-    #   yield builder if block_given?
-    # end
-    # add_cloud_collection(:vms) do |builder|
-    #   builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    # end
   end
-
-  # def add_cloud_collection(name)
-  #   add_collection(cloud, name) do |builder|
-  #     builder.add_properties(:parent => cloud_manager)
-  #     yield builder if block_given?
-  #   end
-  # end
 
 end
