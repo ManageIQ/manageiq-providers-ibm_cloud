ae1caf5c9fb7f32c22bd71535428a9ff49d5e95a
rspradeep <prathapsky9@gmail.com>
2020-09-17 19:58:41 +0530

    WIP

---
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair_spec.rb
@@ -0,0 +1,57 @@
+describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthKeyPair do
+  let(:ems) do
+    FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud, :provider_region => "us-south")
+  end
+  let(:key_pair_attributes) {
+    {
+      :name        => "key1",
+      :fingerprint => "0000",
+      :public_key  => "AAA...B",
+      :private_key => "BBB...C"
+    }
+  }
+  let(:the_raw_key_pair) do
+    double.tap do |key_pair|
+      allow(key_pair).to receive(:name).and_return('key1')
+      allow(key_pair).to receive(:fingerprint).and_return('0000')
+      allow(key_pair).to receive(:public_key).and_return('AAA...B')
+      allow(key_pair).to receive(:private_key).and_return('BBB...C')
+    end
+  end
+
+  describe 'key pair create and delete' do
+    it 'creates new key pair in nova' do
+      service = double
+      key_pairs = double
+      allow(ExtManagementSystem).to receive(:find).with(ems.id).and_return(ems)
+      allow(ems).to receive(:connect).with(:service => 'Compute').and_return(service)
+      allow(service).to receive(:key_pairs).and_return(key_pairs)
+      allow(key_pairs).to receive(:create).with(key_pair_attributes).and_return(
+        the_raw_key_pair)
+      subject.class.create_key_pair(ems.id, key_pair_attributes)
+    end
+
+    it 'deletes existing key pair from nova' do
+      service = double
+      subject.name = 'key1'
+      subject.resource = ems
+      allow(ems).to receive(:connect).with(:service => 'Compute').and_return(service)
+      allow(service).to receive(:delete_key_pair).with('key1')
+      subject.delete_key_pair
+    end
+  end
+
+  describe 'validations' do
+    it 'fails create with invalid parameters' do
+      expect(subject.class.validate_create_key_pair(nil)).to eq(
+        :available => false,
+        :message   => 'The Keypair is not connected to an active Provider')
+    end
+
+    it 'pass create with valid parameters' do
+      expect(subject.class.validate_create_key_pair(ems)).to eq(
+        :available => true,
+        :message   => nil)
+    end
+  end
+end
