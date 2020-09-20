39e491b33db8ef056c8cc1bb66757c9cdc26540d
Jay Carman <jwcarman@us.ibm.com>
2020-09-20 06:58:39 -0500

    Use ':service' ems connect option
    See the connect method defined in
    app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
    We're using the ':service' as the 'options' key to set the connection
    type.

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair.rb
@@ -2,7 +2,7 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthKeyP
   IbmCvsKeyPair = Struct.new(:name, :key_name, :fingerprint, :private_key)
 
   def self.raw_create_key_pair(ext_management_system, create_options)
-    power_iaas = ext_management_system.connect(:target => "PowerIaas")
+    power_iaas = ext_management_system.connect(:service => "PowerIaas")
     kp = power_iaas.create_key_pair(create_options[:name], create_options[:public_key])
     IbmCvsKeyPair.new(kp["name"], kp["name"], nil, nil)
   rescue => err
@@ -22,7 +22,7 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthKeyP
   end
 
   def raw_delete_key_pair
-    power_iaas = resource.connect(:target => "PowerIaas")
+    power_iaas = resource.connect(:service => "PowerIaas")
     power_iaas.delete_key_pair(name)
   rescue => err
     _log.log_backtrace(err)
