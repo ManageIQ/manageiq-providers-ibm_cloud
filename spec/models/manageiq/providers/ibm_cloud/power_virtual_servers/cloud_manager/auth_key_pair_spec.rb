42510decd0ac1d60ee99d1969d2099e003d3de8f
rspradeep <prathapsky9@gmail.com>
2020-09-21 20:34:12 +0530

    Removed Useles lines & updated code

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair_spec.rb
@@ -3,21 +3,12 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthK
     FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud, :provider_region => "us-south")
   end
 
-  let(:the_raw_key_pair) do
-    double.tap do |key_pair|
-      allow(key_pair).to receive(:name).and_return('key1')
-      allow(key_pair).to receive(:fingerprint).and_return('0000')
-      allow(key_pair).to receive(:public_key).and_return('AAA...B')
-      allow(key_pair).to receive(:private_key).and_return('BBB...C')
-    end
-  end
-
   describe 'key pair create and delete' do
     it 'creates new key pair' do
       service = double
       allow(ExtManagementSystem).to receive(:find).with(ems.id).and_return(ems)
       allow(ems).to receive(:connect).with(:service => 'PowerIaas').and_return(service)
-      expect(service).to receive(:create_key_pair).with("new-name", "public-key")
+      expect(service).to receive(:create_key_pair).with("new-name", "public-key").and_return({"name" => "new-name"})
       described_class.create_key_pair(ems.id, :name => "new-name", :public_key => "public-key")
     end
 
@@ -26,7 +17,7 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthK
       subject.name = 'key1'
       subject.resource = ems
       allow(ems).to receive(:connect).with(:service => 'PowerIaas').and_return(service)
-      expect(allow).to receive(:delete_key_pair).with('key1')
+      expect(service).to receive(:delete_key_pair).with('key1')
       subject.delete_key_pair
     end
   end
