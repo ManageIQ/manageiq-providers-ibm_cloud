13422c27a8eb3bbe412e824e152d6f0ea33bccd3
rspradeep <prathapsky9@gmail.com>
2020-09-21 10:26:24 +0530

    Modified compute to powerIaas

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/auth_key_pair_spec.rb
@@ -13,19 +13,19 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthK
   end
 
   describe 'key pair create and delete' do
-    it 'creates new key pair in nova' do
+    it 'creates new key pair' do
       service = double
       key_pairs = double
       allow(ExtManagementSystem).to receive(:find).with(ems.id).and_return(ems)
-      allow(ems).to receive(:connect).with(:service => 'Compute').and_return(service)
+      allow(ems).to receive(:connect).with(:service => 'PowerIaas').and_return(service)
       allow(service).to receive(:key_pairs).and_return(key_pairs)
     end
 
-    it 'deletes existing key pair from nova' do
+    it 'deletes existing key pair' do
       service = double
       subject.name = 'key1'
       subject.resource = ems
-      allow(ems).to receive(:connect).with(:service => 'Compute').and_return(service)
+      allow(ems).to receive(:connect).with(:service => 'PowerIaas').and_return(service)
       allow(service).to receive(:delete_key_pair).with('key1')
       subject.delete_key_pair
     end
@@ -33,11 +33,11 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::AuthK
 
   describe 'validations' do
     it 'fails create with invalid parameters' do
-      expect(subject.class.validate_create_key_pair(nil)).to eq(:available => false,:message   => 'The Keypair is not connected to an active Provider')
+      expect(subject.class.validate_create_key_pair(nil)).to eq(:available => false, :message => 'The Keypair is not connected to an active Provider')
     end
 
     it 'pass create with valid parameters' do
-      expect(subject.class.validate_create_key_pair(ems)).to eq(:available => true,:message   => nil)
+      expect(subject.class.validate_create_key_pair(ems)).to eq(:available => true, :message => nil)
     end
   end
 end
