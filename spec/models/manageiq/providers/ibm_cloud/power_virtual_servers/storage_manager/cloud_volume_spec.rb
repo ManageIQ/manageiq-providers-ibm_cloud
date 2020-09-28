12d2d8f2a270123fecb6bff30108ca003c575b56
rspradeep <prathapsky9@gmail.com>
2020-09-28 16:36:41 +0530

    WIP(added vm.rb file and removed useless lines)

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
@@ -1,6 +1,6 @@
 describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::CloudVolume do
   let(:ems) do
-    FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud, :provider_region => "us-south")
+    FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_storage, :provider_region => "us-south")
   end
   let(:StorageManager) { FactoryBot.create(:ems_IbmCloud_StorageManager, :parent_ems_id => ems_cloud.id) }
   let(:cloud_volume) { FactoryBot.create(:cloud_volume_IbmCloud, :ext_management_system => StorageManager ) }
@@ -8,28 +8,24 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::Clo
   describe "cloud volume operations" do
     context "#delete_volume" do
       it "deletes the cloud volume" do
-        stubbed_responses = {
-          :ec2 => {
-            :delete_volume => {}
-          }
-        }
+        def validate_delete_volume
+          msg = validate_volume
+          return {:available => msg[:available], :message => msg[:message]} unless msg[:available]
+          if status == "in-use"
+            return validation_failed("Delete Volume", "Can't delete volume that is in use.")
+          end
 
-        with_aws_stubbed(stubbed_responses) do
-          expect(cloud_volume.delete_volume).to be_truthy
+          {:available => true, :message => nil}
         end
       end
 
       it "catches error from the provider" do
-        stubbed_responses = {
-          :ec2 => {
-            :delete_volume => "UnauthorizedOperation"
-          }
-        }
-
-        with_aws_stubbed(stubbed_responses) do
-          expect do
-            cloud_volume.delete_volume
-          end.to raise_error(MiqException::MiqVolumeDeleteError)
+        def raw_delete_volume
+          ext_management_system.with_provider_connection(:service => 'PowerIaas') do |power_iaas|
+            power_iaas.delete_volume(ems_ref)
+          end
+        rescue => e
+          _log.error("volume=[#{name}], error: #{e}")
         end
       end
     end
