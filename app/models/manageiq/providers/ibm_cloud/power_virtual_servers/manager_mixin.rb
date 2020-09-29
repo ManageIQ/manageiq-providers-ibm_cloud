2825b998632892b11c00c12e738e235b8697a5bf
Kuldip-Nanda <work.kuldip@gmail.com>
2020-09-29 18:54:26 -0400

    minor corection

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
@@ -109,7 +109,6 @@ module ManageIQ::Providers::IbmCloud::PowerVirtualServers::ManagerMixin
 
       require "ibm-cloud-sdk"
       IBM::Cloud::SDK.logger = $ibmcloud_log
-
       iam = IBM::Cloud::SDK::IAM.new(api_key)
       token = iam.get_identity_token
       power_iaas_service = IBM::Cloud::SDK::ResourceController.new(token).get_resource(pcloud_guid)
