d19d1694513b8e8888ede30e1ee5ce37118efcc0
Brad Blancett <sametimesolutions@yahoo.com>
2020-10-01 09:19:31 -0400

    remove key

--- spec/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/refresher_spec.rb
@@ -1,6 +1,6 @@
 describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
   let(:ems) do
-    api_key = "MEyZR4qH8XfhrwUXzeOJndlk8zNWy9x7jnisPj6nRwdC" 
+    api_key = Rails.application.secrets.ibmcvs.try(:[], :api_key) || "IBMCVS_API_KEY" 
     FactoryBot.create(:ems_ibm_cloud_vpc, :provider_region => "us-east").tap do |ems|
       ems.authentications << FactoryBot.create(:authentication, :auth_key => api_key)
     end
