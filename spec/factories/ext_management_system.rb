acb618f064bf7604f23a13284e43f622dfd18df0
rspradeep <prathapsky9@gmail.com>
2020-10-01 20:04:00 +0530

    WIP(updated files)

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
+  factory :ems_ibm_cloud_power_virtual_servers_cloud,
+          :aliases => ["manageiq/providers/ibm_cloud_power_virtual_servers/cloud_manager"],
+          :class   => "ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager",
+          :parent  => :ems_cloud
+end
+
+FactoryBot.define do
   factory :ems_ibm_cloud_power_virtual_servers_storage,
-          :aliases => ["manageiq/providers/ibm_cloud_power_virtual_servers/storage__manager"],
+          :aliases => ["manageiq/providers/ibm_cloud_power_virtual_servers/storage_manager"],
           :class   => "ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager",
           :parent  => :ems_cloud
 end
