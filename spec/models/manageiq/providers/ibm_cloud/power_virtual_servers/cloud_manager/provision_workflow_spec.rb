0f822b2b18d397450b355d34682d5f72d699b344
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2020-10-06 14:54:27 +0200

    Added additional test to the provision_workflow.spec in PVS

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/provision_workflow_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/provision_workflow_spec.rb
@@ -76,50 +76,50 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Provi
         ]
       )
     values = {
-        :name        => nil,
-        :size        => nil,
-        :shareable   => false,
-        :diskType    => nil,
-        :name_1      => "disk_one",
-        :diskType_1  => "tier1",
-        :shareable_1 => "null",
-        :size_2      => "2",
-        :diskType_2  => "standard-legacy",
-        :name_3      => "disk_three",
-        :size_3      => "3",
-       :diskType_3  => "tier3",
-       :name_4      => "disk_four",
-       :size_4      => "",
-       :diskType_4  => "ssd-legacy",
-       :shareable_4 => true
+      :name        => nil,
+      :size        => nil,
+      :shareable   => false,
+      :diskType    => nil,
+      :name_1      => "disk_one",
+      :diskType_1  => "tier1",
+      :shareable_1 => "null",
+      :size_2      => "2",
+      :diskType_2  => "standard-legacy",
+      :name_3      => "disk_three",
+      :size_3      => "3",
+      :diskType_3  => "tier3",
+      :name_4      => "disk_four",
+      :size_4      => "",
+      :diskType_4  => "ssd-legacy",
+      :shareable_4 => true
    }
    expect(workflow.parse_new_volumes_fields(values))
      .to match_array(
        [
          {
-             :name      => "disk_one",
-             :diskType  => "tier1",
-             :size      => 0,
-             :shareable => false
+           :name      => "disk_one",
+           :diskType  => "tier1",
+           :size      => 0,
+           :shareable => false
          },
          {
-             :size      => 2,
-             :diskType  => "standard-legacy",
-             :shareable => false
+           :size      => 2,
+           :diskType  => "standard-legacy",
+           :shareable => false
          },
          {
-             :name      => "disk_three",
-             :size      => 3,
-             :diskType  => "tier3",
-             :shareable => false
+           :name      => "disk_three",
+           :size      => 3,
+           :diskType  => "tier3",
+           :shareable => false
          },
          {
-             :name      => "disk_four",
-             :size      => 0,
-             :diskType  => "ssd-legacy",
-             :shareable => true
+           :name      => "disk_four",
+           :size      => 0,
+           :diskType  => "ssd-legacy",
+           :shareable => true
          }
-      ]
-    )
+       ]
+     )
   end
 end
