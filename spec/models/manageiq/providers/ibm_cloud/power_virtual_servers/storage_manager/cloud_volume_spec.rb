e042ca2ada04474a7a3591a4bf10e8ac105d927c
rspradeep <prathapsky9@gmail.com>
2020-10-06 18:36:24 +0530

    WIP(updated code)

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
@@ -3,24 +3,18 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::Clo
   let(:ems_storage) { FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_storage, :parent_ems_id => ems_cloud.id) }
   let(:cloud_volume) { FactoryBot.create(:cloud_volume_ibm_cloud_power_virtual_servers, :ext_management_system => ems_storage ) }
 
+  describe "#validate_delete_volume" do
+  context "status is in-use" do
+    let(:status) { "in-use" }
 
-  context "#validate_delete_volume" do
-    it "status is in-use" do
-      expect(cloud_volume).to receive(:status).and_return("in-use")
-      validation = cloud_volume.validate_delete_volume
-      expect(validation[:available]).to be false
+    it "validates unavailable" do
     end
+  end
 
-    it "status is available" do
-      expect(cloud_volume).to receive(:status).and_return("available")
-      validation = cloud_volume.validate_delete_volume
-      expect(validation[:available]).to be true
-    end
+  context "status is available"
+    let(:status) { "available" }
 
-    it "ems is missing" do
-      expect(cloud_volume).to receive(:ext_management_system).and_return(nil)
-      validation = cloud_volume.validate_delete_volume
-      expect(validation[:available]).to be false
+    it "validates available" do
     end
   end
 end
