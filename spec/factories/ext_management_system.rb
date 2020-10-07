dffd8a4953f5dea9d20c4527531e903db97b0361
Jay Carman <jwcarman@us.ibm.com>
2020-10-07 06:52:00 -0500

    Add specs for cloud volume operation validations

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -13,13 +13,6 @@ FactoryBot.define do
 end
 
 FactoryBot.define do
-  factory :ems_ibm_cloud_power_virtual_servers_storage,
-          :aliases => ["manageiq/providers/ibm_cloud_power_virtual_servers/storage_manager"],
-          :class   => "ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager",
-          :parent  => :ems_cloud
-end
-
-FactoryBot.define do
   factory :ems_ibm_cloud_vpc,
           :class  => "ManageIQ::Providers::IbmCloud::VPC::CloudManager",
           :parent => :ems_cloud
--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume_spec.rb
@@ -1,20 +1,115 @@
 describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::CloudVolume do
   let(:ems_cloud) { FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_cloud) }
-  let(:ems_storage) { FactoryBot.create(:ems_ibm_cloud_power_virtual_servers_storage, :parent_ems_id => ems_cloud.id) }
-  let(:cloud_volume) { FactoryBot.create(:cloud_volume_ibm_cloud_power_virtual_servers, :ext_management_system => ems_storage ) }
+  let(:ems_storage) do
+    FactoryBot.create(
+      :ems_ibm_cloud_power_virtual_servers_storage,
+      :parent_manager => ems_cloud
+    )
+  end
+  let(:cloud_volume) do
+    FactoryBot.create(
+      :cloud_volume_ibm_cloud_power_virtual_servers,
+      :ext_management_system => ems_storage,
+      :status                => status,
+      :multi_attachment      => multi_attachment
+    )
+  end
+  let(:status) { "available" }
+  let(:multi_attachment) { false }
 
   describe "#validate_delete_volume" do
-  context "status is in-use" do
-    let(:status) { "in-use" }
+    context "when available" do
+      it "validation passes" do
+        expect(cloud_volume.validate_delete_volume[:available]).to be_truthy
+      end
+    end
 
-    it "validates unavailable" do
+    context "when in-use" do
+      let(:status) { "in-use" }
+
+      it "validation fails" do
+        expect(cloud_volume.validate_delete_volume[:available]).to be_falsy
+      end
+    end
+
+    context "when ems is missing" do
+      let(:ems_storage) { nil }
+
+      it "validation fails" do
+        expect(cloud_volume.validate_delete_volume[:available]).to be_falsy
+      end
     end
   end
 
-  context "status is available"
-    let(:status) { "available" }
+  describe "#validate_attach_volume" do
+    context "when available" do
+      context "multi_attachment enabled" do
+        let(:multi_attachment) { true }
 
-    it "validates available" do
+        it "validation passes" do
+          expect(cloud_volume.validate_attach_volume[:available]).to be_truthy
+        end
+      end
+
+      context "multi_attachment disabled" do
+        let(:multi_attachment) { false }
+
+        it "validation passes" do
+          expect(cloud_volume.validate_attach_volume[:available]).to be_truthy
+        end
+      end
+    end
+
+    context "when in-use" do
+      let(:status) { "in-use" }
+
+      context "multi_attachment enabled" do
+        let(:multi_attachment) { true }
+
+        it "validation passes" do
+          expect(cloud_volume.validate_attach_volume[:available]).to be_truthy
+        end
+      end
+
+      context "multi_attachment disabled" do
+        let(:multi_attachment) { false }
+
+        it "validation fails" do
+          expect(cloud_volume.validate_attach_volume[:available]).to be_falsy
+        end
+      end
+    end
+
+    context "when ems is missing" do
+      let(:ems_storage) { nil }
+
+      it "validation fails" do
+        expect(cloud_volume.validate_attach_volume[:available]).to be_falsy
+      end
+    end
+  end
+
+  describe "#validate_detach_volume" do
+    context "when available" do
+      it "validation fails" do
+        expect(cloud_volume.validate_detach_volume[:available]).to be_falsy
+      end
+    end
+
+    context "when in-use" do
+      let(:status) { "in-use" }
+
+      it "validation passes" do
+        expect(cloud_volume.validate_detach_volume[:available]).to be_truthy
+      end
+    end
+
+    context "when ems is missing" do
+      let(:ems_storage) { nil }
+
+      it "validation fails" do
+        expect(cloud_volume.validate_detach_volume[:available]).to be_falsy
+      end
     end
   end
 end
