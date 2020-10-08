b0baa4016f0bd36de0bcd3d3b1aebfba56e6c7a7
Adam Grare <agrare@redhat.com>
2020-10-08 14:17:29 -0400

    Automatically replace the Bearer auth expiration timestamp
    The ibm-cloud-sdk gem automatically tries to renew an expired Bearer
    token which causes VCR UnhandledHTTPInteractions exceptions 1 hour after
    the vcr cassette is recorded.
    
    We can automatically replace the expiration timestamp with one way in
    the future to mitigate this.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,15 @@ require "manageiq-providers-ibm_cloud"
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::IbmCloud::Engine.root, 'spec/vcr_cassettes')
+  config.before_record do |i|
+    # The ibm-cloud-sdk gem attempts to auto-renew the Bearer token if it
+    # detects that it is expired.  This causes unhandled http interactions
+    # after the expiration time.  We can replace the expiration time with
+    # one way in the future to prevent this.
+    if i.request.uri == "https://iam.cloud.ibm.com/identity/token"
+      body = JSON.parse(i.response.body)
+      body["expiration"] = Date.new(2100, 1, 1).to_time.to_i
+      i.response.body = body.to_json
+    end
+  end
 end
