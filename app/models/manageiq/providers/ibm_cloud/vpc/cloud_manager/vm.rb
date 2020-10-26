e62029650f8aa853549f66358ec0d98c32420dc9
Jared Moore <jaredm@ca.ibm.com>
2020-10-26 16:27:41 -0400

    Add start, stop & pause methods

--- app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/vm.rb
+++ app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/vm.rb
@@ -21,4 +21,36 @@ class ManageIQ::Providers::IbmCloud::VPC::CloudManager::Vm < ManageIQ::Providers
   def self.display_name(number = 1)
     n_('Instance (IBM)', 'Instances (IBM)', number)
   end
+
+  # Send a start action to IBM Cloud. Wait for state to change to started, then update the raw_power_state to the current instance state.
+  def raw_start
+    with_provider_connection() do |vpc|
+      instance = vpc.instances.instance(ems_ref)
+      instance.actions.start
+      _log.info("Starting instance #{ems_ref} in state #{instance.status}.")
+      instance.wait_for(started: true)
+      update!(:raw_power_state => instance.status)
+      _log.info("Started instance #{ems_ref} in state #{instance.status}.")
+    end
+  end
+
+  # IBM Cloud does not support suspend. Hiding it from UI.
+  supports_not :suspend
+
+  # Send a stop action to IBM Cloud. Wait for state to change to stopped, then update the raw_power_state to the current instance state.
+  def raw_stop
+    with_provider_connection() do |vpc|
+      instance = vpc.instances.instance(ems_ref)
+      instance.actions.stop
+      _log.info("Stopping instance #{ems_ref} in state #{instance.status}.")
+      instance.wait_for(started: false)
+      update!(:raw_power_state => instance.status)
+      _log.info("Stopped instance #{ems_ref} in state #{instance.status}.")
+    end
+  end
+
+  # IBM Cloud does not support pause. Using stop since can't unsupport it.
+  def raw_pause
+    raw_stop
+  end
 end
