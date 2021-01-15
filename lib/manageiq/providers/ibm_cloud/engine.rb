775a35a150111be95939d8f22ef20f5cd722980c
Jason Frey <fryguy9@gmail.com>
2021-01-15 11:42:32 -0500

    Move logger initialization and configuration into plugins

--- lib/manageiq/providers/ibm_cloud/engine.rb
+++ lib/manageiq/providers/ibm_cloud/engine.rb
@@ -13,6 +13,14 @@ module ManageIQ
         def self.plugin_name
           _('IBM Cloud Provider')
         end
+
+        def self.init_loggers
+          $ibm_cloud_log ||= Vmdb::Loggers.create_logger("ibm_cloud.log")
+        end
+
+        def self.apply_logger_config(config)
+          Vmdb::Loggers.apply_config_value(config, $ibm_cloud_log, :level_ibm_cloud)
+        end
       end
     end
   end
