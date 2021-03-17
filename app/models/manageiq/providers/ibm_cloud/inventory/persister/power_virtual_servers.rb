33d3616198438c2cb3a4a327de922212fa908afd
Jason Frey <fryguy9@gmail.com>
2021-03-17 14:54:56 -0400

    Merge pull request #152 from agrare/use_core_combined_refresh
    Use the combined child-manager refresh support from core
    
    (cherry picked from commit 5979e542596b9148e6d7649c71200867105e1606)

--- app/models/manageiq/providers/ibm_cloud/inventory/persister/power_virtual_servers.rb
+++ app/models/manageiq/providers/ibm_cloud/inventory/persister/power_virtual_servers.rb
@@ -13,86 +13,30 @@ class ManageIQ::Providers::IbmCloud::Inventory::Persister::PowerVirtualServers <
     "ManageIQ::Providers::IbmCloud::PowerVirtualServers"
   end
 
-  def cloud_manager
-    manager.kind_of?(EmsCloud) ? manager : manager.parent_manager
-  end
-
-  def network_manager
-    manager.kind_of?(EmsNetwork) ? manager : manager.network_manager
-  end
-
-  def storage_manager
-    manager.kind_of?(EmsStorage) ? manager : manager.storage_manager
-  end
-
   private
 
   def initialize_cloud_inventory_collections
-    add_cloud_collection(:availability_zones) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
-    add_cloud_collection(:flavors) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
-    add_cloud_collection(:vms) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
+    add_cloud_collection(:availability_zones)
+    add_cloud_collection(:flavors)
+    add_cloud_collection(:vms)
     add_cloud_collection(:hardwares)
     add_cloud_collection(:disks)
     add_cloud_collection(:operating_systems)
-    add_cloud_collection(:auth_key_pairs) do |builder|
-      builder.add_default_values(
-        :resource_id   => ->(persister) { persister.cloud_manager.id },
-        :resource_type => ->(persister) { persister.cloud_manager.class.base_class }
-      )
-    end
-    add_cloud_collection(:miq_templates) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
+    add_cloud_collection(:auth_key_pairs)
+    add_cloud_collection(:miq_templates)
     add_advanced_settings
   end
 
   def initialize_network_inventory_collections
-    add_network_collection(:cloud_networks) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:cloud_subnets) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:network_ports) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
+    add_network_collection(:cloud_networks)
+    add_network_collection(:cloud_subnets)
+    add_network_collection(:network_ports)
     add_network_collection(:cloud_subnet_network_ports)
   end
 
   def initialize_storage_inventory_collections
-    add_storage_collection(:cloud_volumes) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.storage_manager.id })
-    end
-    add_storage_collection(:cloud_volume_types) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.storage_manager.id })
-    end
-  end
-
-  def add_cloud_collection(name)
-    add_collection(cloud, name) do |builder|
-      builder.add_properties(:parent => cloud_manager)
-      yield builder if block_given?
-    end
-  end
-
-  def add_network_collection(name)
-    add_collection(network, name) do |builder|
-      builder.add_properties(:parent => network_manager)
-      yield builder if block_given?
-    end
-  end
-
-  def add_storage_collection(name)
-    add_collection(storage, name) do |builder|
-      builder.add_properties(:parent => storage_manager)
-      yield builder if block_given?
-    end
+    add_storage_collection(:cloud_volumes)
+    add_storage_collection(:cloud_volume_types)
   end
 
   def add_advanced_settings
--- app/models/manageiq/providers/ibm_cloud/inventory/persister/vpc.rb
+++ app/models/manageiq/providers/ibm_cloud/inventory/persister/vpc.rb
@@ -3,18 +3,6 @@ class ManageIQ::Providers::IbmCloud::Inventory::Persister::VPC < ManageIQ::Provi
   require_nested :NetworkManager
   require_nested :StorageManager
 
-  def cloud_manager
-    manager.kind_of?(EmsCloud) ? manager : manager.parent_manager
-  end
-
-  def network_manager
-    manager.kind_of?(EmsNetwork) ? manager : manager.network_manager
-  end
-
-  def storage_manager
-    manager.kind_of?(EmsStorage) ? manager : manager.storage_manager
-  end
-
   def self.provider_module
     "ManageIQ::Providers::IbmCloud::VPC"
   end
@@ -27,25 +15,14 @@ class ManageIQ::Providers::IbmCloud::Inventory::Persister::VPC < ManageIQ::Provi
   end
 
   def initialize_cloud_inventory_collections
-    add_cloud_collection(:vms) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
+    add_cloud_collection(:vms)
     add_cloud_collection(:hardwares)
     add_cloud_collection(:availability_zones)
     add_cloud_collection(:operating_systems)
     add_cloud_collection(:disks)
-    add_cloud_collection(:auth_key_pairs) do |builder|
-      builder.add_default_values(
-        :resource_id   => ->(persister) { persister.cloud_manager.id },
-        :resource_type => ->(persister) { persister.cloud_manager.class.base_class }
-      )
-    end
-    add_cloud_collection(:miq_templates) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
-    add_cloud_collection(:flavors) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.cloud_manager.id })
-    end
+    add_cloud_collection(:auth_key_pairs)
+    add_cloud_collection(:miq_templates)
+    add_cloud_collection(:flavors)
     add_cloud_collection(:vm_and_miq_template_ancestry)
     add_cloud_collection(:networks)
     add_cloud_collection(:vm_and_template_labels)
@@ -53,48 +30,15 @@ class ManageIQ::Providers::IbmCloud::Inventory::Persister::VPC < ManageIQ::Provi
   end
 
   def initialize_network_inventory_collections
-    add_network_collection(:security_groups) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:cloud_networks) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:cloud_subnets) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:floating_ips) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
-    add_network_collection(:network_ports) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.network_manager.id })
-    end
+    add_network_collection(:security_groups)
+    add_network_collection(:cloud_networks)
+    add_network_collection(:cloud_subnets)
+    add_network_collection(:floating_ips)
+    add_network_collection(:network_ports)
     add_network_collection(:cloud_subnet_network_ports)
   end
 
   def initialize_storage_inventory_collections
-    add_storage_collection(:cloud_volumes) do |builder|
-      builder.add_default_values(:ems_id => ->(persister) { persister.storage_manager.id })
-    end
-  end
-
-  def add_storage_collection(name)
-    add_collection(storage, name) do |builder|
-      builder.add_properties(:parent => storage_manager)
-      yield builder if block_given?
-    end
-  end
-
-  def add_network_collection(name)
-    add_collection(network, name) do |builder|
-      builder.add_properties(:parent => network_manager)
-      yield builder if block_given?
-    end
-  end
-
-  def add_cloud_collection(name)
-    add_collection(cloud, name) do |builder|
-      builder.add_properties(:parent => cloud_manager)
-      yield builder if block_given?
-    end
+    add_storage_collection(:cloud_volumes)
   end
 end
