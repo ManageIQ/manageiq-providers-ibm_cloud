84973800a6591ae220d2f1958685decfbc762200
Jay Carman <jwcarman@us.ibm.com>
2021-04-29 17:27:48 -0500

    Define COS spec secrets and basic refresh
    Add secrets/credential configuration in preparation for refresh specs.

--- spec/models/manageiq/providers/ibm_cloud/object_storage/object_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/object_storage/object_manager/refresher_spec.rb
@@ -2,4 +2,47 @@ describe ManageIQ::Providers::IbmCloud::ObjectStorage::ObjectManager::Refresher 
   it ".ems_type" do
     expect(described_class.ems_type).to eq(:ibm_cloud_object_storage)
   end
+  context "#refresh" do
+    let(:ems) do
+      uid_ems    = "960f1ca0-8a54-402b-9747-f980a84bd312"
+      auth_key   = Rails.application.secrets.ibm_cloud_object_storage[:api_key]
+      access_key = Rails.application.secrets.ibm_cloud_object_storage[:access_key]
+      secret_key = Rails.application.secrets.ibm_cloud_object_storage[:secret_key]
+
+      FactoryBot.create(
+        :ems_ibm_cloud_object_storage_object,
+        :uid_ems         => uid_ems,
+        :provider_region => "us-south"
+      ).tap do |ems|
+        ems.endpoints << FactoryBot.create(
+          :endpoint,
+          :hostname => "https://s3.us-east.cloud-object-storage.appdomain.cloud"
+        )
+        ems.authentications << FactoryBot.create(
+          :authentication,
+          :authtype => 'default',
+          :auth_key => auth_key
+        )
+        ems.authentications << FactoryBot.create(
+          :authentication,
+          :authtype => 'bearer',
+          :userid   => access_key,
+          :password => secret_key
+        )
+      end
+    end
+
+    it "full refresh" do
+      2.times do
+        full_refresh(ems)
+        ems.reload
+      end
+    end
+
+    def full_refresh(ems)
+      VCR.use_cassette(described_class.name.underscore) do
+        EmsRefresh.refresh(ems)
+      end
+    end
+  end
 end
