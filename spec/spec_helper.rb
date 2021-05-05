ee536d5a02c4c08ed58eba823be718ddab988450
Jay Carman <jwcarman@us.ibm.com>
2021-05-05 15:41:58 -0500

    Merge pull request #202 from agrare/fix_vcr_secrets
    Fix the dynamic secret cassette placeholder code
    
    (cherry picked from commit 420c1bbcdb98b3a10d8a6b3daf3cfa01c3905202)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,16 +44,17 @@ VCR.configure do |config|
   # config.debug_logger = $stdout # Keep for debugging tests.
   config.ignore_hosts('codeclimate.com') if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::IbmCloud::Engine.root, 'spec/vcr_cassettes')
+
   config.before_record do |i|
     replace_token_contents(i.response) if i.request.uri == "https://iam.cloud.ibm.com/identity/token"
     vpc_sanitizier(i) if i.request.uri.match?('iaas.cloud.ibm')
   end
 
   secrets = Rails.application.secrets
-  secrets.ibm_cloud_power.keys do |secret|
+  secrets.ibm_cloud_power.each_key do |secret|
     config.define_cassette_placeholder(secrets.ibm_cloud_power_defaults[secret]) { secrets.ibm_cloud_power[secret] }
   end
-  secrets.ibm_cloud_vpc.keys do |secret|
+  secrets.ibm_cloud_vpc.each_key do |secret|
     config.define_cassette_placeholder(secrets.ibm_cloud_vpc_defaults[secret]) { secrets.ibm_cloud_vpc[secret] }
   end
 end
