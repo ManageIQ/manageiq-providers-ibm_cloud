08e6ea6699bc780cf5aae7d1b16e69ec0eafd0b6
Oleg Barenboim <chessbyte@gmail.com>
2021-06-14 11:58:17 -0400

    Remove unused validate_reboot method

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/vm.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/vm.rb
@@ -37,10 +37,6 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Vm < Man
     end
   end
 
-  def validate_reboot
-    validate_vm_control_powered_on
-  end
-
   def self.calculate_power_state(raw_power_state)
     case raw_power_state
     when "ACTIVE"
