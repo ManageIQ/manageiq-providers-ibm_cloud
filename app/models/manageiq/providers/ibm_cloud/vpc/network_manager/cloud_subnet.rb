0d1c974e5997aacc6acd01da8750be23e32127fe
nasark <nasarak_786@hotmail.com>
2021-06-28 09:29:19 -0400

    refactor delete subnet queue

--- app/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_subnet.rb
+++ app/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_subnet.rb
@@ -14,30 +14,12 @@ class ManageIQ::Providers::IbmCloud::VPC::NetworkManager::CloudSubnet < ::CloudS
     end
   end
 
-  def delete_cloud_subnet_queue(userid)
-    task_opts = {
-      :action => "deleting Cloud Subnet for user #{userid}",
-      :userid => userid
-    }
-
-    queue_opts = {
-      :class_name  => self.class.name,
-      :method_name => 'raw_delete_cloud_subnet',
-      :instance_id => id,
-      :priority    => MiqQueue::HIGH_PRIORITY,
-      :role        => 'ems_operations',
-      :zone        => ext_management_system.my_zone,
-      :args        => []
-    }
-
-    MiqTask.generic_action_with_callback(task_opts, queue_opts)
-  end
-
   def raw_delete_cloud_subnet
     with_provider_connection do |connection|
       connection.request(:delete_subnet, :id => ems_ref)
     end
   rescue => err
     _log.error("subnet=[#{name}], error: #{err}")
+    raise
   end
 end
