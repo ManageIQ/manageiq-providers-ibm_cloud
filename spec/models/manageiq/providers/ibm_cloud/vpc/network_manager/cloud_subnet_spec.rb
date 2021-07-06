3589983b40c7ebe7bf30fae53a2ededb423480d6
nasark <nasarak_786@hotmail.com>
2021-07-06 14:31:12 -0400

    support to create cloud subnets for vpc

--- spec/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_subnet_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_subnet_spec.rb
@@ -5,20 +5,45 @@ describe ManageIQ::Providers::IbmCloud::VPC::NetworkManager::CloudSubnet do
     FactoryBot.create(:ems_ibm_cloud_vpc, :provider_region => "us-east")
   end
 
+  let(:cloud_network) do
+    FactoryBot.create(:cloud_network_ibm_cloud_vpc,
+                      :ext_management_system => ems.network_manager)
+  end
+
   let(:cloud_subnet) do
     FactoryBot.create(:cloud_subnet_ibm_cloud_vpc,
                       :ext_management_system => ems.network_manager)
   end
 
-  describe '#raw_delete_cloud_subnet' do
+  describe 'cloud subnet actions' do
     let(:connection) do
       double("ManageIQ::Providers::IbmCloud::CloudTools::Vpc")
     end
 
-    it 'deletes the cloud subnet' do
-      expect(cloud_subnet).to receive(:with_provider_connection).and_yield(connection)
-      expect(connection).to receive(:request).with(:delete_subnet, :id => cloud_subnet.ems_ref)
-      cloud_subnet.raw_delete_cloud_subnet
+    before { allow(ems.network_manager).to receive(:with_provider_connection).and_yield(connection) }
+
+    context '#create_cloud_subnet' do
+      it 'creates the cloud subnet' do
+        expect(connection).to receive(:request).with(:create_subnet,
+                                                     :subnet_prototype => {
+                                                       :vpc             => {
+                                                         :id => cloud_network.ems_ref
+                                                       },
+                                                       :name            => 'test',
+                                                       :ipv4_cidr_block => '10.0.0.0/24'
+                                                     })
+
+        ems.network_manager.create_cloud_subnet({:cloud_network_id => cloud_network.id,
+                                                 :name             => 'test',
+                                                 :cidr             => '10.0.0.0/24'})
+      end
+    end
+
+    context '#raw_delete_cloud_subnet' do
+      it 'deletes the cloud subnet' do
+        expect(connection).to receive(:request).with(:delete_subnet, :id => cloud_subnet.ems_ref)
+        cloud_subnet.raw_delete_cloud_subnet
+      end
     end
   end
 end
