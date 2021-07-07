fc23f5d81bd2c16a79a18c37943c017851793fab
nasark <nasarak_786@hotmail.com>
2021-07-07 11:50:47 -0400

    added vpc create cloud networks

--- spec/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_network_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/vpc/network_manager/cloud_network_spec.rb
@@ -10,36 +10,45 @@ describe ManageIQ::Providers::IbmCloud::VPC::NetworkManager::CloudNetwork do
                       :ext_management_system => ems.network_manager)
   end
 
-  describe '#raw_delete_cloud_network' do
-    before { NotificationType.seed }
-
+  describe 'cloud network actions' do
     let(:connection) do
       double("ManageIQ::Providers::IbmCloud::CloudTools::Vpc")
     end
 
-    it 'deletes the cloud network' do
-      expect(cloud_network).to receive(:with_provider_connection).and_yield(connection)
-      expect(connection).to receive(:request).with(:delete_vpc, :id => cloud_network.ems_ref)
-      cloud_network.raw_delete_cloud_network
+    before { allow(ems.network_manager).to receive(:with_provider_connection).and_yield(connection) }
+
+    context '#create_cloud_network' do
+      it 'creates the cloud network' do
+        expect(connection).to receive(:request).with(:create_vpc, :name => 'test')
+        ems.network_manager.create_cloud_network({:name => 'test'})
+      end
     end
 
-    it 'with cloud subnets' do
-      exception = IBMCloudSdkCore::ApiException.new(
-        :code                  => 409,
-        :error                 => "Delete VPC failed: VPC still contains subnets",
-        :transaction_id        => "1234",
-        :global_transaction_id => "5678"
-      )
-      expect(cloud_network).to receive(:with_provider_connection).and_yield(connection)
-      expect(connection).to receive(:request)
-        .with(:delete_vpc, :id => cloud_network.ems_ref)
-        .and_raise(exception)
+    context '#raw_delete_cloud_network' do
+      before { NotificationType.seed }
 
-      expect { cloud_network.raw_delete_cloud_network }.to raise_error(IBMCloudSdkCore::ApiException)
-      expect(Notification.count).to eq(1)
-      expect(Notification.first)
-        .to have_attributes(:options => {:error_message => exception.to_s,
-                                         :subject       => "[#{cloud_network.name}]"})
+      it 'deletes the cloud network' do
+        expect(connection).to receive(:request).with(:delete_vpc, :id => cloud_network.ems_ref)
+        cloud_network.raw_delete_cloud_network
+      end
+
+      it 'with cloud subnets' do
+        exception = IBMCloudSdkCore::ApiException.new(
+          :code                  => 409,
+          :error                 => "Delete VPC failed: VPC still contains subnets",
+          :transaction_id        => "1234",
+          :global_transaction_id => "5678"
+        )
+        expect(connection).to receive(:request)
+          .with(:delete_vpc, :id => cloud_network.ems_ref)
+          .and_raise(exception)
+
+        expect { cloud_network.raw_delete_cloud_network }.to raise_error(IBMCloudSdkCore::ApiException)
+        expect(Notification.count).to eq(1)
+        expect(Notification.first)
+          .to have_attributes(:options => {:error_message => exception.to_s,
+                                           :subject       => "[#{cloud_network.name}]"})
+      end
     end
   end
 end
