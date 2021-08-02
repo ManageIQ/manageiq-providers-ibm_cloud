a1da8b213cb3edfc14bdcdb4e4c463a24c7a410f
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2021-08-02 17:09:57 +0200

    allowing only 1 image import for each PVS and PVC

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import_workflow/import_dispatcher.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import_workflow/import_dispatcher.rb
@@ -1,10 +1,21 @@
 class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageImportWorkflow::ImportDispatcher < ::Job::Dispatcher
-  def dispatch
-    jobs_by_ems, = Benchmark.realtime_block(:pending_import_jobs) { pending_import_jobs }
 
-    jobs_by_ems.each do |ems_id, jobs|
-      free = true # TODO: here determine if there is a free slot
-      do_dispatch(job, ems_id) if free
+  def dispatch
+    pending, = Benchmark.realtime_block(:pending_import_jobs) { pending_import_jobs }
+    running, = Benchmark.realtime_block(:running_import_jobs) { running_import_jobs }
+    busy_src_ids = running.map { |_, dst| dst.map { |job| job[:options][:src_provider_id] } }.flatten
+
+    pending.each do |dst_id, pending_jobs|
+      return if running[dst_id].present?
+      job = pending_jobs.detect { |x| busy_src_ids.exclude?(x[:options][:src_provider_id]) }
+      busy_src_ids << job[:options][:src_provider_id]
+      do_dispatch(job) unless job.nil?
+    end
+  end
+
+  def running_import_jobs
+    running_jobs.each_with_object(Hash.new { |h, k| h[k] = [] }) do |job, h|
+      h[job.options[:ems_id]] << job
     end
   end
 
@@ -14,7 +25,7 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageImp
     end
   end
 
-  def do_dispatch(job, ems_id)
+  def do_dispatch(job)
     MiqQueue.put_unless_exists(
       :args        => [:start],
       :class_name  => "Job",
