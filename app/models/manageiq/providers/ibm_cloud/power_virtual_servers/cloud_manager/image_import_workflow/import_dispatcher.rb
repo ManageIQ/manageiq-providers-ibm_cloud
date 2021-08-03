fd8a9a626f97d463abb45be3d8100b217ed0a319
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2021-08-03 17:44:54 +0200

    refactoring in image import dispatcher
    - simplified 'busy_src_ids' search
    - minor code style issues

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import_workflow/import_dispatcher.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/image_import_workflow/import_dispatcher.rb
@@ -3,26 +3,25 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageImp
   def dispatch
     pending, = Benchmark.realtime_block(:pending_import_jobs) { pending_import_jobs }
     running, = Benchmark.realtime_block(:running_import_jobs) { running_import_jobs }
-    busy_src_ids = running.map { |_, dst| dst.map { |job| job[:options][:src_provider_id] } }.flatten
+    busy_src_ids = running.flat_map{ |_, dst| dst.map { |job| job.options[:src_provider_id] }}.uniq
 
     pending.each do |dst_id, pending_jobs|
-      return if running[dst_id].present?
-      job = pending_jobs.detect { |x| busy_src_ids.exclude?(x[:options][:src_provider_id]) }
-      busy_src_ids << job[:options][:src_provider_id]
-      do_dispatch(job) unless job.nil?
+      break if running[dst_id].present?
+      pending_job = pending_jobs.detect { |pending_job| busy_src_ids.exclude?(pending_job.options[:src_provider_id]) }
+
+      if not pending_job.nil?
+        busy_src_ids << pending_job.options[:src_provider_id]
+        do_dispatch(pending_job)
+      end
     end
   end
 
   def running_import_jobs
-    running_jobs.each_with_object(Hash.new { |h, k| h[k] = [] }) do |job, h|
-      h[job.options[:ems_id]] << job
-    end
+    running_jobs.group_by { |job| job.options[:ems_id] }
   end
 
   def pending_import_jobs
-    pending_jobs.each_with_object(Hash.new { |h, k| h[k] = [] }) do |job, h|
-      h[job.options[:ems_id]] << job
-    end
+    pending_jobs.group_by { |job| job.options[:ems_id] }
   end
 
   def do_dispatch(job)
@@ -32,7 +31,6 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::ImageImp
       :instance_id => job.id,
       :method_name => "signal",
       :priority    => MiqQueue::HIGH_PRIORITY,
-      :role        => "smartstate",
       :task_id     => job.guid,
       :zone        => job.zone
     )
