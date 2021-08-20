b479d8757692070fdfd3ce1965e92e531b40c48a
Oleg Barenboim <chessbyte@gmail.com>
2021-08-20 11:25:59 -0400

    Use SupportsFeatureMixin for attach_volume and detach_volume

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/storage_manager/cloud_volume.rb
@@ -2,6 +2,14 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::CloudV
   supports :create
   supports_not :snapshot_create
   supports_not :update
+  supports :attach_volume do
+    unsupported_reason_add(:attach_volume, _("the volume is not connected to an active Provider")) unless ext_management_system
+    unsupported_reason_add(:attach_volume, _("cannot attach non-shareable volume that is in use.")) if status == "in-use" && !multi_attachment
+  end
+  supports :detach_volume do
+    unsupported_reason_add(:detach_volume, _("the volume is not connected to an active Provider")) unless ext_management_system
+    unsupported_reason_add(:detach_volume, _("the volume status is '%{status}' but should be 'in-use'") % {:status => status}) unless status == "in-use"
+  end
 
   def available_vms
     availability_zone.vms.select { |vm| vm.format == volume_type }
@@ -153,17 +161,6 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::CloudV
     _log.error("volume=[#{name}], error: #{e}")
   end
 
-  def validate_attach_volume
-    msg = validate_volume_available
-    return {:available => msg[:available], :message => msg[:message]} unless msg[:available]
-
-    if status == "in-use" && !multi_attachment
-      return validation_failed(_("Attach Volume"), _("Can't attach non-shareable volume that is in use."))
-    end
-
-    {:available => true, :message => nil}
-  end
-
   def raw_attach_volume(vm_ems_ref, _device = nil)
     with_provider_connection(:service => 'PCloudVolumesApi') do |api|
       api.pcloud_pvminstances_volumes_post(cloud_instance_id, vm_ems_ref, ems_ref)
@@ -171,10 +168,6 @@ class ManageIQ::Providers::IbmCloud::PowerVirtualServers::StorageManager::CloudV
   rescue => e
     _log.error("volume=[#{name}], error: #{e}")
     raise MiqException::MiqVolumeAttachError, _("Unable to attach volume: %{error_message}") % {:error_message => e.message}
-  end
-
-  def validate_detach_volume
-    validate_volume_in_use
   end
 
   def raw_detach_volume(vm_ems_ref)
