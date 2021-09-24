ef27628f451bf01febb832305c0ae6ece9740e28
nasark <nasarak_786@hotmail.com>
2021-09-24 15:16:34 -0400

    added rake task for refresh setup

--- spec/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/refresher_spec.rb
@@ -5,38 +5,40 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
 
   let(:ems) do
     api_key = Rails.application.secrets.ibm_cloud_vpc[:api_key]
-    FactoryBot.create(:ems_ibm_cloud_vpc, :provider_region => "us-east").tap do |ems|
+    FactoryBot.create(:ems_ibm_cloud_vpc, :provider_region => "us-south").tap do |ems|
       ems.authentications << FactoryBot.create(:authentication, :auth_key => api_key)
     end
   end
 
-  # If recording a new VCR note that this method, assert_ems_counts and assert_specific_vm will need to be updated.
+  # If recording a new VCR note that this method and assert_specific_vm will need to be updated.
   # The first time run there may be failures for key_pairs in VM or IP Address in assert_specific_floating_ip.
   # This has to do with obscuring code in the vcr config. Try again to see if running this with a recorded VCR fixes the issue.
-  it "tests the refresh" do
-    2.times do
-      with_vcr { ems.refresh }
-      ems.reload
+  context "full refresh" do
+    it "tests the refresh", :full_refresh => true do
+      inventory = nil
+      2.times do
+        with_vcr { ems.refresh }
+        inventory ? assert_inventory_not_changed { inventory } : inventory = serialize_inventory
+        ems.reload
 
-      assert_ems_counts
-      assert_specific_vm
-      assert_specific_flavor
-      assert_specific_resource_group
-      assert_specific_security_group
-      assert_specific_cloud_volume_type
-      assert_specific_cloud_subnet
-      assert_specific_floating_ip
-      assert_specific_cloud_database
-      assert_specific_cloud_database_flavor
-      assert_vm_labels
+        assert_specific_flavor
+        assert_specific_security_group
+        assert_specific_cloud_volume
+        assert_specific_cloud_volume_type
+        assert_specific_cloud_subnet
+        assert_specific_floating_ip
+        assert_specific_cloud_database
+        assert_specific_cloud_database_flavor
+        assert_specific_vm
+      end
     end
   end
 
   context "targeted refresh" do
     before { with_vcr { ems.refresh } }
 
-    context "vm target" do
-      let(:target) { ems.vms.find_by(:ems_ref => "0757_81687d4a-4676-4eeb-9fd7-55f9c7fffb69") }
+    context "vm target", :target_vm => true do
+      let(:target) { ems.vms.find_by(:name => "rake-instance") }
 
       it "with a deleted vm" do
         connection = double("IbmCloud::CloudTool")
@@ -66,50 +68,20 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
     end
   end
 
-  # Test that the refresh has persisted the same number of items as expected from the Cloud.
-  def assert_ems_counts
-    # Cloud Manager
-    cloud_manager = {
-      :availability_zones => 3,
-      :key_pairs          => 2,
-      :miq_templates      => 57,
-      :resource_groups    => 3,
-      :vms                => 6,
-    }.freeze
-    check_counts(ems, cloud_manager)
-
-    # Network Manager
-    network_manager = {
-      :cloud_networks  => 2,
-      :cloud_subnets   => 4,
-      :floating_ips    => 5,
-      :security_groups => 2,
-    }.freeze
-    check_counts(ems, network_manager)
-
-    # Storage Manager
-    storage_manger = {
-      :cloud_volume_types => 4,
-      :cloud_volumes      => 15,
-    }
-    check_counts(ems, storage_manger)
-  end
-
   # Test a specific VMs's configuration.
   def assert_specific_vm
-    vm = check_resource_fetch(ems, :vms, '0777_249ba858-a4eb-4f2c-ba6c-72254a781d0d')
-
-    check_count(vm, :ipaddresses, 1)
-    expect(vm.availability_zone.name).to eq('us-east-3')
+    vm = ems.vms.find_by(:name => 'rake-instance')
+    check_count(vm, :ipaddresses, 2)
+    expect(vm.availability_zone.name).to eq('us-south-1')
     expect(vm.cpu_total_cores).to eq(2)
-    expect(vm.hardware.memory_mb).to eq(16_384)
+    expect(vm.hardware.memory_mb).to eq(8_192)
     expect(vm.hardware.cpu_total_cores).to eq(2)
     expect(vm.hardware.cpu_sockets).to eq(2)
     expect(vm.hardware.bitness).to eq(64)
-    expect(vm.operating_system[:product_name]).to eq('linux_redhat')
-    expect(vm.flavor.name).to eq('mx2-2x16')
-    expect(vm.raw_power_state).to eq('stopped')
-    expect(vm.power_state).to eq('off')
+    expect(vm.operating_system[:product_name]).to eq('linux_debian')
+    expect(vm.flavor.name).to eq('bx2-2x8')
+    expect(vm.raw_power_state).to eq('running')
+    expect(vm.power_state).to eq('on')
     expect(vm.security_groups.count).to eq(1)
 
     ## linking key pairs to vms
@@ -123,12 +95,12 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
   end
 
   def assert_specific_flavor
-    flavor = check_resource_fetch(ems, :flavors, 'mx2-2x16')
+    flavor = check_resource_fetch(ems, :flavors, 'bx2-2x8')
     expect(flavor).to have_attributes(
-      :name            => 'mx2-2x16',
+      :name            => 'bx2-2x8',
       :cpu_total_cores => 2,
-      :memory          => 18_432,
-      :ems_ref         => 'mx2-2x16',
+      :memory          => 8_192,
+      :ems_ref         => 'bx2-2x8',
       :type            => 'ManageIQ::Providers::IbmCloud::VPC::CloudManager::Flavor',
       :enabled         => true
     )
@@ -141,39 +113,46 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
     check_attribute_values(resource, '29b1dd25de2d40b5ae5bd5f719f30db8', class_type, 'camc-test')
   end
 
-  # Test a resource_group record is properly persisted.
+  # Test a vm_label record is properly persisted.
   def assert_vm_labels
-    vm = check_resource_fetch(ems, :vms, '0777_f73e8687-3813-465f-99df-ba6e4ee8f289')
-    check_count(vm, :labels, 4)
+    vm = ems.vms.find_by(:name => 'rake-instance')
+    check_count(vm, :labels, 1)
   end
 
   # Test a security_group record is properly persisted.
   def assert_specific_security_group
-    resource = check_resource_fetch(ems, :security_groups, 'r014-e4be0c69-6df6-4464-a9bc-384e4179ea1b')
+    resource = ems.security_groups.find_by(:name => 'rake-group')
     class_type = 'ManageIQ::Providers::IbmCloud::VPC::NetworkManager::SecurityGroup'
-    check_attribute_values(resource, 'r014-e4be0c69-6df6-4464-a9bc-384e4179ea1b', class_type, 'backup-deglazed-bagful-deflation')
+    check_attribute_values(resource, class_type, 'rake-group')
 
-    cloud_network = check_resource_fetch(ems, :cloud_networks, 'r014-0fa2acc6-2a41-4f2b-9c89-bcea07cdcbc3')
+    cloud_network = ems.cloud_networks.find_by(:name => 'rake-network')
     check_relationship(resource, :cloud_network_id, cloud_network)
+  end
+
+  # Test a cloud_volume record is properly persisted.
+  def assert_specific_cloud_volume
+    resource = ems.cloud_volumes.find_by(:name => 'rake-vol')
+    class_type = 'ManageIQ::Providers::IbmCloud::VPC::StorageManager::CloudVolume'
+    check_attribute_values(resource, class_type, 'rake-vol', {:size => 10.gigabytes, :status => 'available'})
   end
 
   # Test a cloud_volume_type record is properly persisted.
   def assert_specific_cloud_volume_type
     resource = check_resource_fetch(ems, :cloud_volume_types, 'general-purpose')
     class_type = 'ManageIQ::Providers::IbmCloud::VPC::StorageManager::CloudVolumeType'
-    check_attribute_values(resource, 'general-purpose', class_type, 'general-purpose', {:description => 'tiered'})
+    check_attribute_values(resource, class_type, 'general-purpose', {:description => 'tiered'})
   end
 
   # Test the components of a cloud subnet.
   def assert_specific_cloud_subnet
-    cloud_subnet = check_resource_fetch(ems, :cloud_subnets, '0757-ef523a2f-5356-42ff-8a78-9325509465b9')
+    cloud_subnet = ems.cloud_subnets.find_by(:name => 'rake-subnet')
 
     # Test cloud_network relationship.
-    cloud_network = check_resource_fetch(ems, :cloud_networks, 'r014-0fa2acc6-2a41-4f2b-9c89-bcea07cdcbc3')
+    cloud_network = ems.cloud_networks.find_by(:name => 'rake-network')
     check_relationship(cloud_subnet, :cloud_network_id, cloud_network)
 
     # Test availability_zone relationship.
-    availability_zone = check_resource_fetch(ems, :availability_zones, 'us-east-1')
+    availability_zone = check_resource_fetch(ems, :availability_zones, 'us-south-1')
     check_relationship(cloud_subnet, :availability_zone_id, availability_zone)
 
     # Test remaining fields.
@@ -181,40 +160,36 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
     ip_version = 'ipv4'
 
     additional_values = {:cidr => '127.0.0.0/24', :ip_version => ip_version, :network_protocol => ip_version}
-    check_attribute_values(cloud_subnet, '0757-ef523a2f-5356-42ff-8a78-9325509465b9', class_type, 'b-subneet-washington-dc-1', additional_values)
+    check_attribute_values(cloud_subnet, class_type, 'rake-subnet', additional_values)
   end
 
   # Test a floating_ip record.
 
   def assert_specific_floating_ip
-    floating_ip = check_resource_fetch(ems, :floating_ips, 'r014-1892eee4-79ab-4d26-8efd-4c5759fc03fc')
+    vm = ems.vms.find_by(:name => 'rake-instance')
+    floating_ip = ems.floating_ips.find_by(:vm_id => vm.id)
 
-    vm = check_resource_fetch(ems, :vms, '0777_a9ee9e6a-231a-4a3c-b9cb-fc83d25114a2')
     check_relationship(floating_ip, :vm_id, vm)
 
     # Find the network ports using the vm ems_ref which is stored as device_ref on network_ports.
     # Assumes only 1 network port is on the VM.
-    network_port = check_resource_fetch(vm, :network_ports, '0777_a9ee9e6a-231a-4a3c-b9cb-fc83d25114a2', :key => :device_ref)
+    network_port = check_resource_fetch(vm, :network_ports, 'eth0', :key => :name)
     check_relationship(floating_ip, :network_port_id, network_port)
 
     internal_ip_address = vm.ipaddresses.first
     class_type = "ManageIQ::Providers::IbmCloud::VPC::NetworkManager::FloatingIp"
-    values = {:type => class_type, :ems_ref => 'r014-1892eee4-79ab-4d26-8efd-4c5759fc03fc', :status => 'available'}
+    values = {:type => class_type, :status => 'available'}
     expect(floating_ip).to have_attributes(values)
 
-    check_obscured_ip(floating_ip, :address, '150.239.208.80')
     check_obscured_ip(floating_ip, :fixed_ip_address, internal_ip_address.to_s)
   end
 
   def assert_specific_cloud_database
-    cloud_database = ManageIQ::Providers::IbmCloud::VPC::CloudManager::CloudDatabase.find_by(:name => "gsaPointProcPostgreSQLDR")
+    cloud_database = ManageIQ::Providers::IbmCloud::VPC::CloudManager::CloudDatabase.find_by(:name => "rake-db")
     expect(cloud_database).to have_attributes(
-      :ems_ref      => "79f86763-5d06-445c-a847-47351667401b",
-      :name         => "gsaPointProcPostgreSQLDR",
-      :status       => "active",
-      :db_engine    => "11",
-      :used_storage => 10_737_418_240,
-      :max_storage  => 7_696_581_394_432
+      :name      => "rake-db",
+      :status    => "active",
+      :db_engine => "12"
     )
   end
 
@@ -261,9 +236,8 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
   # @param name [String] The value of the name attribute.
   # @param additional_values [Hash] Values that are unique to the class.
   # @return [void]
-  def check_attribute_values(resource, ems_ref, class_type, name = nil, additional_values = {})
-    default_values = {:ems_ref => ems_ref, :type => class_type.to_s}
-    default_values[:name] = name unless name.nil?
+  def check_attribute_values(resource, class_type, name, additional_values = {})
+    default_values = {:type => class_type.to_s, :name => name}
     values = default_values.merge(additional_values)
     expect(resource).to have_attributes(values)
   end
@@ -288,13 +262,5 @@ describe ManageIQ::Providers::IbmCloud::VPC::CloudManager::Refresher do
     expect(resource).to respond_to(method.to_sym), "ems does not respond to #{method}"
     actual = resource.send(method.to_sym).count
     expect(actual).to eq(expected), "Resource #{resource.class.name}.#{method} has #{actual} items expected #{expected}"
-  end
-
-  # Test a number of resource counts.
-  # @param resource [ApplicationRecord] The instance that holds the relationship.
-  # @param resources [Hash<Symbol, Integer>] A hash where the key represents a method and the value represents the expected count.
-  # @return [void]
-  def check_counts(resource, resources)
-    resources.each_pair { |key, value| check_count(resource, key, value) }
   end
 end
