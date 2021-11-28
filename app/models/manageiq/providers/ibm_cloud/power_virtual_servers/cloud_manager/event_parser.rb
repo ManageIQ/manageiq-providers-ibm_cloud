68a9cfbe91d96ad780aacd7728bfedb1ae3810b6
Jay Carman <jwcarman@us.ibm.com>
2021-11-28 06:39:00 -0600

    Handle 'update' and 'create' events cases
    Both 'update' and 'create' events have cases where there isn't an
    existing VM to refresh. (TODO: Instead a full EMS refresh is
    initiated.)

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/event_parser.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/event_parser.rb
@@ -23,7 +23,19 @@ module ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::EventPa
     pvm_instance_name = event[:message].split('\'')[1]
     ems = ExtManagementSystem.find(event_hash[:ems_id])
     vm = ems.vms.find_by(:name => pvm_instance_name)
-    return if vm.nil?
+
+    if vm.nil? && ['update', 'create'].include?(event['type'])
+
+      # TODO: If event type is 'pvm-instance.update' and the VM name was
+      #       changed then only the new name is given in the event message :(
+      #       Should we refresh the entire EMS on this event? Same question
+      #       for a 'pvm-instance.create'.
+      event_hash
+    else
+      return
+    end
+
+    sleep(60)
 
     event_hash[:vm_uid_ems] = vm.uid_ems
     event_hash[:vm_ems_ref] = vm.ems_ref
