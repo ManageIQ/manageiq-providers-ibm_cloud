6986480f1469d38735bac37bf7409059d90e1ec6
Amogh Singh <amogh.singh@ibm.com>
2022-02-04 14:59:41 -0600

    Overrides subclass regions for powerVS.

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
@@ -37,17 +37,9 @@ module ManageIQ::Providers::IbmCloud::PowerVirtualServers::ManagerMixin
   end
 
   def api_endpoint_url(location)
-    api_endpoint_overrides = ::Settings.ems.ems_ibm_cloud_power_virtual_servers.api_endpoint_overrides
-
-    if api_endpoint_overrides.key?(location.to_sym)
-      url = api_endpoint_overrides[location.to_sym]
-    else
-      region = location.sub(/-\d$/, '')
-      region = region.sub(/\d\d$/, '')
-      url = "#{region}.power-iaas.cloud.ibm.com"
-    end
-
-    url
+    region = location.sub(/-\d$/, '')
+    region = region.sub(/\d\d$/, '')
+    ManageIQ::Providers::IbmCloud::PowerVirtualServers::Regions.regions[region][:hostname]
   end
 
   def verify_credentials(_auth_type = nil, options = {})
