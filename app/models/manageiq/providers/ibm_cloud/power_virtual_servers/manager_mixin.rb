138c519949ca7728356564b805e46492fcd1635a
Amogh Singh <amogh.singh@ibm.com>
2022-02-16 08:54:09 -0600

    Made rubocop edits and added us-east to yaml file.

--- app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
+++ app/models/manageiq/providers/ibm_cloud/power_virtual_servers/manager_mixin.rb
@@ -37,8 +37,8 @@ module ManageIQ::Providers::IbmCloud::PowerVirtualServers::ManagerMixin
   end
 
   def api_endpoint_url(location)
-    region = location.sub(/-\d$/, '')
-    region = region.sub(/\d\d$/, '')
+    region = location.sub(/-\d$/, '')  #e.g. 'eu-de-2' -> 'eu-de'
+    region = region.sub(/\d\d$/, '')   #e.g. 'syd04' -> 'syd'
     ManageIQ::Providers::IbmCloud::PowerVirtualServers::Regions.regions[region][:hostname]
   end
 
