dd04ccb2e1e2be1c4332d88548ee96c543c0463d
nasark <nasarak_786@hotmail.com>
2022-04-08 16:01:10 -0400

    create cloud db support for vpc

--- app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/cloud_database.rb
+++ app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/cloud_database.rb
@@ -1,5 +1,20 @@
 class ManageIQ::Providers::IbmCloud::VPC::CloudManager::CloudDatabase < ::CloudDatabase
+  supports :create
   supports :delete
+
+  def self.raw_create_cloud_database(ext_management_system, options)
+    ext_management_system.with_provider_connection do |connection|
+      resource_group = ext_management_system.resource_groups.find_by!(:name => options[:resource_group_name])
+      connection.resource.controller.request(:create_resource_instance,
+                                             :name             => options[:name],
+                                             :target           => ext_management_system.provider_region,
+                                             :resource_group   => resource_group.ems_ref,
+                                             :resource_plan_id => "databases-for-#{options[:database]}-standard")
+    end
+  rescue => err
+    _log.error("cloud database=[#{name}], error: #{err}")
+    raise
+  end
 
   def raw_delete_cloud_database
     with_provider_connection do |connection|
