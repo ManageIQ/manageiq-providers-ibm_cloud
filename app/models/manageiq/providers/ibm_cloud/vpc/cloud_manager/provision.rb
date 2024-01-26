0d68b78d4d37ce88b17f31b12c80eb3d24f1c20d
Joe Rafaniello <jrafanie@gmail.com>
2024-01-26 10:57:34 -0500

    Remove include_concern as it's no longer needed with zeitwerk

--- app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/provision.rb
+++ app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/provision.rb
@@ -3,8 +3,7 @@
 # Opts into CloudManager provisioning. Custom logic is separated into module mixins.
 class ManageIQ::Providers::IbmCloud::VPC::CloudManager::Provision < ::MiqProvisionCloud
   include ManageIQ::Providers::IbmCloud::VPC::CloudManager::LoggingMixin # Standardise the logging.
-
-  include_concern 'Cloning'       # Actual provision to cloud.
-  include_concern 'Payload'       # Create json payload.
-  include_concern 'StateMachine'  # Pre-provision tasks.
+  include Cloning       # Actual provision to cloud.
+  include Payload       # Create json payload.
+  include StateMachine  # Pre-provision tasks.
 end
--- app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/provision_workflow.rb
+++ app/models/manageiq/providers/ibm_cloud/vpc/cloud_manager/provision_workflow.rb
@@ -3,12 +3,11 @@
 # Class contains all logic used to populate the UI.
 class ManageIQ::Providers::IbmCloud::VPC::CloudManager::ProvisionWorkflow < ::MiqProvisionCloudWorkflow
   include ManageIQ::Providers::IbmCloud::VPC::CloudManager::LoggingMixin # Standardise the logging.
-
-  include_concern 'Common'  # Provides common functionality.
-  include_concern 'General' # Used for general options.
-  include_concern 'Network' # Used for network options.
-  include_concern 'Volumes' # Used for volume options.
-  include_concern 'Fields'  # Used for manipulating field hashes.
+  include Common  # Provides common functionality.
+  include General # Used for general options.
+  include Network # Used for network options.
+  include Volumes # Used for volume options.
+  include Fields  # Used for manipulating field hashes.
 
   # Class methods. Do not move to sub module.
   class << self
