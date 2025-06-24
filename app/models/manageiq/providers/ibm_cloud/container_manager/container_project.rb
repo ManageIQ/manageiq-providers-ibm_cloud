1781d7bf1550ee7e1eca95cfdf066f79d49489e1
Adam Grare <adam@grare.com>
2025-06-24 11:22:46 -0400

    Add ContainerProject Subclass

---
+++ app/models/manageiq/providers/ibm_cloud/container_manager/container_project.rb
@@ -0,0 +1,4 @@
+ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject.include(ActsAsStiLeafClass)
+
+class ManageIQ::Providers::IbmCloud::ContainerManager::ContainerProject < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerProject
+end
