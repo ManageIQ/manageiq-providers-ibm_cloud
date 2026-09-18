d9202e22423c32078f2bcb7fa87b97af98e8b0a5
lu-ramya <lu.ramya@partner.ibm.com>
2026-09-18 06:45:43 +0000

    fixed vm configuration button

--- spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/vm_spec.rb
+++ spec/models/manageiq/providers/ibm_cloud/power_virtual_servers/cloud_manager/vm_spec.rb
@@ -39,8 +39,15 @@ describe ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::Vm do
     end
 
     context "with :resize" do
-      let(:state) { :resize }
-      include_examples "Vm operation is available when not powered on"
+      it "when powered on" do
+        vm.update(:raw_power_state => power_state_on)
+        expect(vm.supports?(:resize)).to be_truthy
+      end
+
+      it "when not powered on" do
+        vm.update(:raw_power_state => power_state_suspended)
+        expect(vm.supports?(:resize)).to be_truthy
+      end
     end
 
     context "with :remove_snapshot" do
